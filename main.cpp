b5ea6cb4ca1327651bd9859ba03ebfb57ac446e8
Michail Chulanov <stud204056@vyatsu.ru>
2025-06-07 23:32:45 +0300

    Добавлено использование XML-сервиса

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include "JsonService.h"
+#include "XmlService.h"
+
+int main() {
+    JsonService js;
+    Weather w1 = js.getWeather("weather.json");
+
+    XmlService xs;
+    Weather w2 = xs.getWeather("weather.xml");
+}
+
